224ab927f3f57853f3093c09ee83c082e0aae000
Matheus Barros <mdbf42@gmail.com>
2020-11-03 23:35:39 -0300

    Docs @ Added Swagger

--- src/controllers/GetCityPlaylistDTO.ts
+++ src/controllers/GetCityPlaylistDTO.ts
@@ -1,7 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString, Length } from "class-validator";
 
 export class GetCityPlaylist {
     
+    @ApiProperty({ 
+        type: String, 
+        description: 'The name of any City',
+        minLength: 2,
+        maxLength: 100
+    })
+
     @IsString()
     @IsNotEmpty()
     @Length(2, 100)
--- src/framework/controllers/city/city.controller.ts
+++ src/framework/controllers/city/city.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ICityPlaylistUseCase } from 'src/business/city/interfaces/ICityPlaylistUseCase.interface';
 import { CityPlaylistUseCase } from 'src/business/city/useCases/CityPlaylistUseCase';
 import { GetCityPlaylist } from 'src/controllers/GetCityPlaylistDTO';
 
 @Controller('city')
+@ApiTags('City Playlist')
 export class CityController {
 
     constructor(
